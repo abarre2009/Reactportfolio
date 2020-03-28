4928d90c6d2722bc7b3c75137cca9c83fab4db01
abdul Barre <abarre2009@gmail.com>
2020-03-28 10:24:02 -0500

    added link to image

--- client/src/skills.js
+++ client/src/skills.js
@@ -4,7 +4,14 @@ import "./App.css";
 function Skills() {
   return (
     <div className="App">
-      <h3>Skills</h3>
+      <h1>Skills</h1>
+      <h2>HTML/CSS</h2>
+      <h2>Bootstrap</h2>
+      <h2>Vanilla Java script</h2>
+      <h2>Jquery</h2>
+      <h2>React</h2>
+      <h2>Node</h2>
+      <h2>Express</h2>
     </div>
   );
 }
