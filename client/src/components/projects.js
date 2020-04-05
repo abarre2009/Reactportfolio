3e19708f86740d8766e346e8fc4555c090e19160
abdul Barre <abarre2009@gmail.com>
2020-04-05 18:33:26 -0500

    updated each app info in projects page

--- client/src/components/projects.js
+++ client/src/components/projects.js
@@ -25,7 +25,12 @@ function Projects() {
               "url(https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT25ZU4SEpMmV6IgLuXL1ZRKJtbeHAK4TP76pv_POgoBGIZ1eZl) center / cover"
           }}
         ></CardTitle>
-        <CardText>Favorite Giphy Storage App</CardText>
+        <CardText>
+          <b>
+            Favorite Giphy Storage App is an app where you can search and save
+            your favorite Gif.
+          </b>
+        </CardText>
         <CardActions border>
           <Button href={"https://giphyapp017co.herokuapp.com"} colored>
             Live Demo
@@ -48,7 +53,12 @@ function Projects() {
               "url(https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT_TlwaQGJi_HDB-yDSAXBBuqkTQImj4KUp5PHZZcrquknDHaqj) center / cover"
           }}
         ></CardTitle>
-        <CardText>Employee Directory App</CardText>
+        <CardText>
+          <b>
+            Employee directory app that allows you to search for employees in
+            the system.
+          </b>
+        </CardText>
         <CardActions border>
           <Button href={"https://morning-lake-75256.herokuapp.com/"} colored>
             Live Demo
@@ -71,7 +81,12 @@ function Projects() {
               "url(https://image.shutterstock.com/image-photo/gym-day-schedule-pencil-260nw-553690546.jpg) center / cover"
           }}
         ></CardTitle>
-        <CardText>Day Planner App</CardText>
+        <CardText>
+          <b>
+            Day Planner App is an app where you can set your schedule for the
+            day
+          </b>
+        </CardText>
         <CardActions border>
           <Button
             href={"https://abarre2009.github.io/Homework05/index.html"}
@@ -102,7 +117,12 @@ function Projects() {
               "url(https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRP_HDgI3TJN073VHRLaPKUjRox4CEiX85IGhadkKqApiFI4Nlg) center / cover"
           }}
         ></CardTitle>
-        <CardText>Voting Info App</CardText>
+        <CardText>
+          <b>
+            Voting Info App is an application that can be used to find voting
+            info in your area such as candidates and polling station{" "}
+          </b>
+        </CardText>
         <CardActions border>
           <Button
             href={
